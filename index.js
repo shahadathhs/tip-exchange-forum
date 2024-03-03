1f7f67fa7632587a2bf76f272455eea3f3ac0cd9
Shahadath Hossen Sajib <shahadathhossensajib732@gmail.com>
2024-03-03 22:09:38 +0600

    complete read section functional

--- index.js
+++ index.js
@@ -4,28 +4,30 @@ let read = parseInt(readNum);
 const loadAllBlogs = async () => {
   const response = await fetch("https://openapi.programming-hero.com/api/retro-forum/posts");
   const data = await response.json();
+  
   const div = document.getElementById("blogs-div");
   div.innerHTML = "";
+  
   data.posts.forEach((blog) => {
     const blogDiv = document.createElement("div");
     blogDiv.innerHTML = `
     <div class="flex flex-col lg:flex-row justify-around items-center gap-3 bg-[#797DFC1A] rounded-2xl p-5">
-      <div>
-        <div class="avatar online">
-          <div class="w-24 rounded-full">
+        <div>
+          <div class="avatar online">
+            <div class="w-24 rounded-full">
             <img src= ${blog.image} />
           </div>
-        </div>
-        <div class="avatar offline hidden">
-          <div class="w-24 rounded-full">
+          </div>
+          <div class="avatar offline hidden">
+            <div class="w-24 rounded-full">
             <img src=${blog.image} />
           </div>
-        </div>
+      </div>
       </div>
       <div class="space-y-2">
         <div class="flex gap-3 text-gray-400"><p>#${blog.category}</p><p>Author : ${blog.author.name}</p></div>
         <div>
-          <h3 class="font-extrabold">${blog.title}</h3>
+          <h3 class="font-extrabold off">${blog.title}</h3>
           <p class="text-gray-400">${blog.description}</p>
           <hr class="border-dashed border-t-2 m-3">
         </div>
@@ -44,14 +46,11 @@ const loadAllBlogs = async () => {
     `;
     div.appendChild(blogDiv);
   });
-  
   const btns = document.getElementsByClassName("read-btns");
   for (const btn of btns) {
     btn.addEventListener("click", function () {
       const readT = btn.parentNode.parentNode.childNodes[3].childNodes[1].innerText;
-      console.log(readT);
       const readC = btn.parentNode.childNodes[1].childNodes[3].childNodes[1].innerText;
-      console.log(readC);
 
       const readTt = document.getElementById("read-title");
       const p1 = document.createElement("p");
